6bcb7e76a4eb0ca883c45703da373d268d2b339f
Zihao Wang <dev@wzhd.org>
2015-09-20 11:46:07 +0800

    Import the class Clipping from Clipping.py.

--- kindlescraper.py
+++ kindlescraper.py
@@ -1,36 +1,5 @@
 import argparse
-
-class Clipping:
-    
-    type = ""
-    title = ""
-    page = 0
-    location = 0
-    dateTimeAdded = ""
-    content = ""
-    
-    def __init__(self, chunk):
-        chunkLines = chunk.split('\n')
-        secondLine = chunkLines[2].strip()
-        secondLineSplit = secondLine.split(" ")
-        
-        #title
-        self.title = chunkLines[1].strip()
-        
-        #type
-        self.type = secondLineSplit[1]
-        
-        #page
-        self.page = secondLineSplit[4]
-        
-        #location
-        self.location = secondLineSplit[7]
-        
-        # date & time
-        self.dateTimeAdded = secondLine.split("Added on ")[-1]
-        
-        if (self.type != "Bookmark"):
-            self.content = chunkLines[-2]
+from Clipping import Clipping
 
 p = argparse.ArgumentParser(description='scrap kindle notes')
 p.add_argument('filename', help='path to a text file in kindle format')
