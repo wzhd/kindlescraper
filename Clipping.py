252a052aa08ee43115ce0b2dd3acccf4539d1de7
Zihao Wang <dev@wzhd.org>
2015-09-20 13:43:14 +0800

    Rewrite Clipping.py.

--- Clipping.py
+++ Clipping.py
@@ -1,32 +1,46 @@
+def parserecord(recordinfo):
+    """parse the type and page or position of a record"""
+    # TODO
+    return(recordinfo, recordinfo, recordinfo)
+
+def parsetime(timeinfo):
+    """get time from a string written in some languages"""
+    # For example: 作成日: 2014年10月15日水曜日 14:43:29
+    # Another example Hinzugefügt am Freitag, 23. Januar 2015 09:30:51
+    # TODO
+    return(timeinfo)
+
 class Clipping:
     
     type = ""
     title = ""
     page = 0
     location = 0
+    recordinfo = None
+    timeinfo = None
     dateTimeAdded = ""
     content = ""
     
     def __init__(self, chunk):
         chunkLines = chunk.split('\n')
-        secondLine = chunkLines[2].strip()
-        secondLineSplit = secondLine.split(" ")
-        
+        chunkLines =[l.strip() for l in chunkLines if len(l) > 0]
+
         #title
-        self.title = chunkLines[1].strip()
-        
-        #type
-        self.type = secondLineSplit[1]
-        
-        #page
-        self.page = secondLineSplit[4]
-        
-        #location
-        self.location = secondLineSplit[7]
-        
+        self.title = chunkLines[0]
+
+        infoLine = chunkLines[1]
+
+        # Perhaps the format changed. The separator is "|" now.
+        infos = infoLine.split("|")
+        # Usually there is only one "|" in a line, but sometimes ther are two.
+        # Such as "- Ihre Notiz auf Seite 17 | bei Position 270 | Hinzugefügt..."
+        self.recordinfo = ''.join(infos[:-1])
+        self.timeinfo = infos[-1]
+
+        #type and page or position
+        self.type, self.page, self.location = parserecord(self.recordinfo)
+
         # date & time
-        self.dateTimeAdded = secondLine.split("Added on ")[-1]
+        self.dateTimeAdded = parsetime(self.timeinfo)
         
-        if (self.type != "Bookmark"):
-            self.content = chunkLines[-2]
-            
+        self.content = chunkLines[2:]
